073f7b72a22eb5d759916cdd2cb59211d0de5e33
Zarinabonu <33760567+Zarinabonu@users.noreply.github.com>
2022-09-27 17:43:14 +0500

    crud v2

--- src/main/java/com/example/walletapp/repo/WalletRepo.java
+++ src/main/java/com/example/walletapp/repo/WalletRepo.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface WalletRepo extends JpaRepository<Wallet, Long> {
 
     List<Wallet> findAllByOrderByPriority();
-    List<Wallet> findAllByOrdeOrderById();
+    List<Wallet> findAllByOrderById();
 
 }
