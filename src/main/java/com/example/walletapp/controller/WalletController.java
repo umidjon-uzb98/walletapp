bf9c592e55baa8c1b241f9be52983b4c073e8855
lordu98 <u.makhmudov211326@gmail.com>
2022-09-27 21:45:41 +0500

    home

--- src/main/java/com/example/walletapp/controller/WalletController.java
+++ src/main/java/com/example/walletapp/controller/WalletController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/wallet")
 @RequiredArgsConstructor
+@CrossOrigin
 public class WalletController {
 
     private final WalletService walletService;
